a58dcfc9b59dd9bf152132c66301cd01a393f989
Parunchai Keawkhampa <parunchxi@gmail.com>
2024-05-27 23:47:47 +0700

    ✨ (testing.h): Add sorting test function.

--- include/testing.h
+++ include/testing.h
@@ -13,6 +13,8 @@ typedef struct TestArray
 // Utility functions for testing
 TestArray *new_test_array(int test_id, int size, int type);
 void save_test_array(TestArray *test_array);
+void save_sorting_runtimes(TestArray *test_array, int algorithm, int runtimes[]);
+void test_sorting_algorithms(int test_id, int size, int type);
 int get_bubble_sort_runtime(int arr[], int n);
 int get_selection_sort_runtime(int arr[], int n);
 int get_insertion_sort_runtime(int arr[], int n);
