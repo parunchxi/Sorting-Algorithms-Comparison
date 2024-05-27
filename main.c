600c575b8dcf50f01ae0fef8d7564e4785908d1d
Parunchai Keawkhampa <parunchxi@gmail.com>
2024-05-28 00:47:20 +0700

    ✨ (main.c): Update main program code.

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "include/sorting.h"
+#include "include/testing.h"
 
 int main()
 {
-    printf("Hello, world!\n");
+    int id = START_ID;
+    for (int size = START_SIZE; size < END_SIZE; size += SIZE_STEP)
+    {
+        // Random array
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+            test_sorting_algorithms(id, size, 0);
+            id++;
+        }
+        // Sorted array
+        test_sorting_algorithms(id, size, 1);
+        id++;
+        // Reversed sorted array
+        test_sorting_algorithms(id, size, 2);
+        id++;
+    }
+    printf("\e[0;32mTesting complete\n\e[0m");
     return 0;
 }
