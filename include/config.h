6fe741dfb81ffe2d3f542c81bade2e94d8a6a53f
Parunchai Keawkhampa <parunchxi@gmail.com>
2024-05-27 23:46:57 +0700

    ✨ (config.h): Add custom configuration file.

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int START_ID = 1;     // Initial ID for testing
+const int START_SIZE = 1;   // Initial array size for testing
+const int END_SIZE = 10000; // Maximum array size for testing
+const int SIZE_STEP = 1;    // Increment in array size for each test
+const int NUM_TESTS = 10;   // Number of tests for each array size (random array)
+const int NUM_SORTS = 5;    // Number of sorts for each algorithm on each test array
+
+#endif
